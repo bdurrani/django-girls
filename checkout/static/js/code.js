e3c5ed1ffb3f06fc7c0614a42703ded8ac3f2131
Bilal Durrani <bilal33+cloud9@hotmail.com>
2017-12-24 03:14:30 +0000

    Got to update the table via POST

--- checkout/static/js/code.js
+++ checkout/static/js/code.js
@@ -1,24 +1,6 @@
 (function() {
     $(document).ready(function() {
 
-        function clickcell(ev) {
-            const dataset = ev.currentTarget.dataset;
-            if (dataset) {
-                let url = dataset.url;
-                fetch(url, {
-                        headers: {
-                            'X-Requested-With': 'XMLHttpRequest'
-                        }
-                    })
-                    .then(response => {
-                        console.log(response);
-                    })
-                    .catch(function(reason) {
-                        console.log(reason);
-                    });
-            }
-        }
-
         function clickButton(ev) {
             console.log('editor button click');
             ev.preventDefault();
@@ -42,13 +24,6 @@
                         if (data.operation === 'checkout') {}
                         else {}
                     });
-                // .done(function() {
-                //     alert("second success");
-                // })
-                // .fail(function() {
-                //     alert("error");
-                // })
-
             }
         }
 
--- checkout/views.py
+++ checkout/views.py
@@ -3,38 +3,33 @@ from .models import Elvis
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 
 @login_required
 def unit_list(request):
     units = Elvis.objects.all()
     current_user = request.user
-    print('hit unit_list %s' % (request.method))
     # import pdb; pdb.set_trace()
     return render(request, 'checkout/unit_list.html', 
     {'units': units, 'inuse': Elvis.IN_USE})
     
-def update_availability1(request):
+def update_availability(request):
     unitid = -1
     userid = -1
     # import pdb; pdb.set_trace() 
     if request.method == "POST":
         units = Elvis.objects.all()
-        print("This was a POST")
         unitid = request.POST.get('unitid')
         userid = request.POST.get('userid')
         unit = Elvis.objects.get(id=unitid)
         user = User.objects.get(id=userid)
         if(unit.current_user == user):
-            print('user already reserved the device. Check in device')
             checkin_device(unit)
         else:
-            print('checking out device')
             checkout_device(unit, user)
         unit.save()
-        return HttpResponseRedirect('/checkout')
-        # return render(request, 'checkout/unit_list.html',  {'units': units, 'inuse': Elvis.IN_USE})
+        return HttpResponseRedirect(reverse('unit_list'))
         
-    print('This was a GET')
     unitid = request.GET['unitid']
     userid = request.GET['userid']
         
@@ -43,10 +38,8 @@ def update_availability1(request):
     unit = Elvis.objects.get(id=unitid)
     operation = 'checkin'
     if(unit.current_user == user):
-        print('user already reserved the device. Check in device')
         checkin_device(unit)
     else:
-        print('checking out device')
         checkout_device(unit, user)
         operation = 'checkout'
     unit.save()
